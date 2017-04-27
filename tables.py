8e6a59375ecfcff4eb7bbf94b992559a4d59c486
Eric Murta <ericmurta@hotmail.com>
2017-04-27 11:45:01 -0300

    finished tables

--- tables.py
+++ tables.py
@@ -17,6 +17,7 @@ class Cards(db.Model):
 	artist = db.Column(db.String)
 
 	def __init__(self, name, manaCost, colors, types, rarity, text, artist):
+		
 		self.name = name
 		self.manaCost = manaCost
 		self.colors = colors
@@ -24,3 +25,36 @@ class Cards(db.Model):
 		self.rarity = rarity
 		self.text = text
 		self.artist = artist
+
+# TABLE FOR USERS
+
+class Users(db.Model):
+
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String, unique = True)
+	email = db.Column(db.String, unique = True)
+	cards = db.relationship('Cards', backref = 'users', lazy = 'dynamic')
+	clan = db.Column(db.Integer, db.ForeignKey('clan.id'))
+
+	def __init_(self,username,email,cards,clan):
+
+		self.username = username
+		self.email = email
+		self.cards = cards
+		self.clan = clan
+
+# TABLE FOR CLANS
+
+class Clans(db.Model):
+	id = db.Column(db.String, primary_key = True)
+	name = db.Column(db.String, unique = True)
+	usernames = db.relationship('Users', backref = 'clans', lazy = 'dynamic')  
+
+	def __init__(self,name,usernames):
+
+		self.name = name
+		self.usernames = usernames
+
+
+# ONE USER HAS MANY CARDS BUT ONE CLAN
+# ONE CLAN HAS MANY USERS
