cc0e7b79b44ace471a2d7f319ca023da9beeda83
Eric Murta <ericmurta@hotmail.com>
2017-04-28 12:29:00 -0300

    adding cards to a specific user

--- routes.py
+++ routes.py
@@ -13,22 +13,38 @@ def health_check():
 def add_cards():
 	
 	# SCRIPT FOR ADDING CARDS TO THE DB
-	 for cards in data['cards']:
+	for cards in data['cards']:
 	 	dbcreate = Cards(cards['id'],cards['name'],cards['manaCost'],str(cards['colors']),str(cards['types']),cards['rarity'],cards['text'],cards['artist'])
 	 	db.session.add(dbcreate)
 		db.session.commit()
-	 return 'added'
-	# user_input = request.get_json()
-	# card_name = user_input['name']
-	# card_db_filter = Cards.query.filter_by(name = card_name).first()
-	# user_username = user_input['username']
-	# user_email = user_input['email']
-	# dbcreate = Users(user_username, user_email, card_db_filter)
+	return 'added'
 
-	# db.session.add(dbcreate)
-	# db.session.commit()
+@app.route('/adduser', methods = ['POST'])
+def add_user():
 
-	# return 'Card name %s added' % card_db_filter.name
+	user_input = request.get_json()
+	user_username = user_input['username']
+	user_email = user_input['email']
+	dbcreate = Users(user_username, user_email)
+
+	db.session.add(dbcreate)
+	db.session.commit()
+
+	return 'User %s added' % user_username
+
+# ADD CARDS TO A SPECIFIC USER
+
+@app.route('/addcard/<username>', methods = ['POST'])
+def add_card_to_user(username):
+
+	user_input = request.get_json()
+	card_name = user_input['name']
+	card = Cards.query.filter_by(name = card_name).first()
+	user = Users.query.filter_by(username = username).first()
+	card.owner.append(user)
+	db.session.commit()
+	return '%s added to %s' % (card_name,username)
+
 
 @app.route('/delete/<name>', methods = ['DELETE'])
 def delete_card(name):
