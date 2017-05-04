188db76e4522169ab938ba6185c7a976cd35c655
Eric Murta <ericmurta@hotmail.com>
2017-05-04 11:27:16 -0300

    removed unnecessary imported files

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ migrate = Migrate(app,db)
 
 from tables import Cards,Users
 import routes
-import migrate
-import loadjsoncards
-import allsingletypes
+
 
 # To initialize db, open python shell 
 #from application import db -> db.create_all()
