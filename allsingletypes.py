ec13659cc1f84c4b1ed8a48a221ca0f750ca926a
Eric Murta <ericmurta@hotmail.com>
2017-05-11 11:02:26 -0300

    implemented empty values

--- allsingletypes.py
+++ allsingletypes.py
@@ -8,9 +8,12 @@ subtypes_flatten = ()
 for type in types:
 	types_flatten += tuple(type)
 types_flatten = list(set(types_flatten))
+types_flatten.append("empty")
 
 for subtype in subtypes:
 	subtypes_flatten += tuple(subtype)
 subtypes_flatten = list(set(subtypes_flatten))
+subtypes_flatten.append("empty")
+
 
 
--- migrate.py
+++ migrate.py
@@ -7,28 +7,49 @@ import re
 
 # THIS ROUTE IS ONLY FOR POPULATING DB WITH CARD FROM JSONFILE
 
+# CARDS
  	
 for card in data.values():
 	card_name = card.get('name', '')
 	card_mana_cost = re.sub("\W", "",card.get('manaCost', ''))
-	card_mana_cost_split = []
-	for word in card_mana_cost:
-		card_mana_cost_split.append(word)
+	if card_mana_cost != '':
+		card_mana_cost = sorted(card_mana_cost)
+		card_mana_cost = ''.join(card_mana_cost)
 	card_text = card.get('text', '')
-	dbcreate = Cards(card_name,str(sorted((card_mana_cost_split))), 	  		             
+	dbcreate = Cards(card_name,card_mana_cost, 	  		             
  		             card_text)
   	db.session.add(dbcreate)
  	db.session.commit()
+# --------------------------------------------------------------
+
+# COLORS
 
 colors = ["Red",
 		  "Green",
 		  "Blue",
 		  "Black",
-		  "White"]
+		  "White",
+		  "empty"]
+
 for color in colors:
 	dbcreate = Colors(color)
 	db.session.add(dbcreate)
 	db.session.commit()
+
+for card in data.values():
+	colors = card.get('colors', [])
+	name = card.get('name','')
+	table_card = Cards.query.filter_by(name = name).first()
+	for color in colors:
+		color_table = Colors.query.filter_by(color = color).first()
+		table_card.colors_ref.append(color_table)
+	else:
+		color_table = Colors.query.filter_by(color = "empty").first()
+		table_card.colors_ref.append(color_table)
+	db.session.commit()
+# --------------------------------------------------------------
+
+# CLANS
 
 clans = ["Azorius",
 		 "Dimir",
@@ -40,20 +61,14 @@ clans = ["Azorius",
 		 "Golgari",
 		 "Boros",
 		 "Simic"]
+
 for clan in clans:
 	dbcreate = Clans(clan)
 	db.session.add(dbcreate)
 	db.session.commit()
+# --------------------------------------------------------------
 
-for card in data.values():
-	colors = card.get('colors', None)
-	name = card.get('name','')
-	table_card = Cards.query.filter_by(name = name).first()
-	if colors != None:
-		for color in colors:
-			color_table = Colors.query.filter_by(color = color).first()
-			table_card.colors_ref.append(color_table)
-			db.session.commit()
+# TYPES
 
 for type in types_flatten:
 	dbcreate = Types(type)
@@ -61,14 +76,19 @@ for type in types_flatten:
 	db.session.commit()
 
 for card in data.values():
-	types = card.get('types', None)
+	types = card.get('types', [])
 	name = card.get('name', '')
 	table_card = Cards.query.filter_by(name = name).first()
-	if types != None:
-		for type in types:
-			type_table = Types.query.filter_by(types = type).first()
-			table_card.types_ref.append(type_table)
-			db.session.commit()
+	for type in types:
+		type_table = Types.query.filter_by(types = type).first()
+		table_card.types_ref.append(type_table)
+	else:
+		type_table = Types.query.filter_by(types = "empty").first()
+		table_card.types_ref.append(type_table)
+	db.session.commit()
+# --------------------------------------------------------------
+
+# SUBTYPES
 
 for subtype in subtypes_flatten:
 	dbcreate = Subtypes(subtype)
@@ -77,14 +97,16 @@ for subtype in subtypes_flatten:
 
 
 for card in data.values():
-	subtypes = card.get('subtypes', None)
+	subtypes = card.get('subtypes', [])
 	name = card.get('name', '')
 	table_card = Cards.query.filter_by(name = name).first()
-	if subtypes != None:
-		for subtype in subtypes:
-			subtype_table = Subtypes.query.filter_by(subtype = subtype).first()
-			table_card.subtypes_ref.append(subtype_table)
-			db.session.commit()
+	for subtype in subtypes:
+		subtype_table = Subtypes.query.filter_by(subtype = subtype).first()
+		table_card.subtypes_ref.append(subtype_table)
+	else:
+		subtype_table = Subtypes.query.filter_by(subtype = "empty").first()
+		table_card.subtypes_ref.append(subtype_table)
+	db.session.commit()
 
 
 
